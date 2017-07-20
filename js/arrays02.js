cb8be283091d5026aded359032dc3f907b90a10c
noribang <hoonk9387@gmail.com>
2017-07-19 19:55:46 -0500

    add .map() for arrays in arrays02.js

--- js/arrays02.js
+++ js/arrays02.js
@@ -14,3 +14,33 @@ summands.forEach(function(summand) {
 })
 console.log("Total: " + total);
 
+/* ======================================== */
+/* Double every item in array. */
+/* ======================================== */
+var numbers = [1, 2, 3, 4, 5];
+
+var doubleNumbers = [];
+numbers.forEach(function(number) {
+	doubleNumbers.push(number * 2);
+	console.log(number + " * 2 = " + doubleNumbers);
+});
+
+console.log(doubleNumbers);
+
+/* 3. .map() does not change original array but returns new array. */
+/* Loops through every item in array and does something to it. */
+/* ======================================== */
+/* Doubles every item in array. */
+/* ======================================== */
+var numbers = [1,2,3,4,5];
+var double; // or var double = [];
+
+var doubleNumbers = numbers.map(function(number) {
+	double = number * 2;
+	console.log(double);
+	return double;
+});
+
+console.log(doubleNumbers);
+
+
