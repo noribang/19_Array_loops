d223550e2958f027b5aa648e10ee883c0661e091
noribang <hoonk9387@gmail.com>
2017-07-19 21:49:49 -0500

    add code for arrays using For Loops originally written with .forEach()

--- js/arrays02.js
+++ js/arrays02.js
@@ -44,4 +44,57 @@ var doubleNumbers = numbers.map(function(number) {
 console.log(doubleNumbers);
 console.log("Original array: " + numbers);
 
+/* 4. for Loop */
+/* Syntax: for (initialization; condition; final expression) { 
+	               code to be executed}*/
+/* Example */
+for (var index = 1; index <= 3; index += 1) {
+	console.log("index = " + index);
+}
+
+/* Original written with .forEach() */
+var languages = ['HTML', 'CSS', 'Javascript'];
+languages.forEach(function(language) {
+	console.log(language);
+});
+
+var total = 0;
+var summands = [1,2,3,4,5];
+summands.forEach(function(summand) {
+	console.log("total + summand = " + total + " + " + summand);
+	total += summand;
+	console.log("Subtotal: " + total);
+})
+console.log("Total: " + total);
+
+/* ======================================== */
+/* Rewritten using For Loop */
+/* ======================================== */
+var languages = ['HTML', 'CSS', 'Javascript'];
+for (var index = 0; index < languages.length; index += 1) {
+	console.log("index = " + index);
+	console.log("Element at index " + index + " = " +languages[index]);
+}
+
+var total = 0;
+var summands = [1,2,3,4,5];
+var temp_total;
+for (var index = 0; index < summands.length; index += 1) {
+	temp_total = total;
+	total += summands[index];
+	console.log(temp_total + " + " + summands[index] + " = " + total);
+}
+
+console.log(total);
+
+/* .length property */
+var languages = ['HTML', 'CSS', 'Javascript'];
+languages.length;
+3
+
+var school = "epicodus";
+school.length;
+8
+
+
 
